7d6b67853841e7caa3a118b85f6f6e5edd1dd6c7
Gu-Miao <1450206741@qq.com>
2021-12-23 13:42:05 +0800

    feat: add linter and formatter

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from '@nabla/vite-plugin-eslint'
 import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   resolve: { alias: [{ find: /@\/(.*)/, replacement: path.join(__dirname, 'src', '$1') }] },
-  plugins: [react()],
+  plugins: [react(), eslint()]
 })
